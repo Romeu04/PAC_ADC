e5e944da914d83362bd4374ad9043dcfc6cb2660
KEVIN CHEDID EL AZAR <kevin.azal@catolicasc.edu.br>
2024-06-20 21:46:24 -0300

    Começo da criação do bando de dados

---
+++ Back-End/models.py
@@ -0,0 +1,50 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint, ForeignKeyConstraint
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost/mydb'
+db = SQLAlchemy(app)
+
+class Produtos(db.Model):
+    __tablename__ = 'Produtos'
+    idProduto = db.Column(db.Integer, primary_key=True)
+    nomeProduto = db.Column(db.String(45), nullable=False)
+    estoqueProduto = db.Column(db.Integer, nullable=False)
+    foto = db.Column(db.LargeBinary, nullable=False)
+    precoProduto = db.Column(db.Float, nullable=False)
+
+class NiveisUsuarios(db.Model):
+    __tablename__ = 'NiveisUsuarios'
+    idNiveisUsuarios = db.Column(db.Integer, primary_key=True)
+    nomeNivel = db.Column(db.String(45), nullable=False)
+
+class Membros(db.Model):
+    __tablename__ = 'Membros'
+    idMembros = db.Column(db.Integer, primary_key=True)
+    nomeMembro = db.Column(db.String(45), nullable=False)
+    sobrenomeMembro = db.Column(db.String(45), nullable=False)
+    dataNascimento = db.Column(db.Date, nullable=False)
+    fotoMembro = db.Column(db.LargeBinary)
+    nomeLogin = db.Column(db.String(45), nullable=False, unique=True)
+    senhaLogin = db.Column(db.String(45), nullable=False, unique=True)
+    NiveisUsuarios_idNiveisUsuarios = db.Column(db.Integer, db.ForeignKey('NiveisUsuarios.idNiveisUsuarios'), nullable=False)
+
+class Agendamentos(db.Model):
+    __tablename__ = 'Agendamentos'
+    idAgendamentos = db.Column(db.Integer, primary_key=True)
+    diaAgendamento = db.Column(db.Date, nullable=False)
+    dataInicio = db.Column(db.Date, nullable=False)
+    dataTermino = db.Column(db.Date, nullable=False)
+    descricaoAgendamento = db.Column(db.String(255), nullable=False)
+    Membros_idMembros = db.Column(db.Integer, db.ForeignKey('Membros.idMembros'), nullable=False)
+
+class logProdutos(db.Model):
+    __tablename__ = 'logProdutos'
+    idlogProdutos = db.Column(db.Integer, primary_key=True)
+    dataAlteracao = db.Column(db.Date, nullable=False)
+    Produtos_codProduto = db.Column(db.Integer, db.ForeignKey('Produtos.idProduto'), nullable=False)
+    Membros_idMembros = db.Column(db.Integer, db.ForeignKey('Membros.idMembros'), nullable=False)
+
+# Cria as tabelas
+db.create_all()
