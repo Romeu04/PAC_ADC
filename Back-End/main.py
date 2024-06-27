de254b766a2733c274c7a8c9e24ca9eb16fc3b0a
VitorLescowicz <vitorlesco@gmail.com>
2024-06-26 21:34:47 -0300

    Adicionada Funcao de Login

--- Back-End/main.py
+++ Back-End/main.py
@@ -1,6 +1,7 @@
 import io
 
-from flask import Flask, render_template, request, jsonify, send_from_directory, send_file
+from flask import Flask, render_template, request, jsonify, send_from_directory, send_file, redirect, session, url_for
+
 from database import db
 from CRUD import *
 import os
@@ -10,42 +11,70 @@ template_dir = os.path.join(parent_dir, 'Front-End')
 static_dir = os.path.join(parent_dir, 'Front-End', 'static')
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BD_Atletica.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'super_secret_key'
 
 db.init_app(app)
 
+def login_required(f):
+    def decorated_function(*args, **kwargs):
+        if 'loggedin' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    decorated_function.__name__ = f.__name__
+    return decorated_function
 
-@app.route('/login')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        account = Membros.query.filter_by(emailLogin=username, senhaLogin=password).first()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account.idMembros
+            session['username'] = account.nomeMembro
+            return redirect(url_for('agenda'))
+        else:
+            msg = 'Credenciais incorretas!'
+    return render_template('login.html', msg=msg)
+
+
 
 @app.route('/estoque.html')
+@login_required
 def estoque():
     return render_template('estoque.html')
 
 @app.route('/membros.html')
+@login_required
 def membros():
     return render_template('membros.html')
 
 @app.route('/agenda.html')
+@login_required
 def agenda():
     return render_template('agenda.html')
 
 @app.route('/get_all_products')
+@login_required
 def all_products():
     products = get_all_products()
     return jsonify([product.to_dict() for product in products])
 
 @app.route('/get_all_members')
+@login_required
 def all_members():
     products = get_all_members()
     print(products)
     return jsonify([product.to_dict() for product in products])
 
 @app.route('/update-stock', methods=['POST'])
+@login_required
 def update_stock():
+
     data = request.get_json()
     productId = data.get('productId')
     newQuantity = data.get('newQuantity')
@@ -54,7 +83,9 @@ def update_stock():
 
     return 'Estoque atualizado com sucesso!', 200
 
+
 @app.route('/add_product', methods=['POST'])
+@login_required
 def add_product():
     data = request.form
     nomeProduto = data.get('nomeProduto')
@@ -78,6 +109,7 @@ def images(productId):
         return send_file(io.BytesIO(product.foto), mimetype='image/jpeg')
     else:
         return "No image found for product", 404
+
 
 @app.route('/imagem_membro/<int:memberId>')
 def member_images(memberId):
